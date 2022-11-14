5c5c837401460c4646bf97595a594d922aebc86f
hafijur-rahman1 <hafijur.info24@gmail.com>
2022-11-15 02:16:58 +0600

    some animate add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Task from "./Task";
+import TaskForm from "./TaskForm";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 
 function App() {
+  //
+  const [tasks, setTask] = useState([]);
+  const [animationParent] = useAutoAnimate({ duration: 1000 });
+
+  //
+  useEffect(() => {
+    if (tasks.length === 0) return;
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+  //
+  useEffect(() => {
+    const tasks = JSON.parse(localStorage.getItem("tasks"));
+    setTask(tasks || []);
+  }, []);
+  //
+  const addTask = (name) => {
+    setTask((pre) => {
+      return [{ name: name, done: false }, ...pre];
+    });
+  };
+  //
+  const updateTaskDone = (taskIndex, newDone) => {
+    setTask((pre) => {
+      const newTask = [...pre];
+      newTask[taskIndex].done = newDone;
+      return newTask;
+    });
+  };
+  //
+  const removeTask = (taskIndexRemove) => {
+    setTask((pre) => {
+      return pre.filter((taskName, index) => index !== taskIndexRemove);
+    });
+  };
+  //
+
+  const numberCompleted = tasks.filter((t) => t.done).length;
+  const total = tasks.length;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main ref={animationParent}>
+      <h2>
+        {numberCompleted}/{total} Completed
+      </h2>
+      <TaskForm onAdd={addTask} />
+
+      {tasks.map((task, index) => (
+        <Task
+          {...task}
+          key={index}
+          onToggle={(done) => updateTaskDone(index, done)}
+          onTrash={() => removeTask(index)}
+        />
+      ))}
+    </main>
   );
 }
 
